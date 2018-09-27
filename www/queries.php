c7d0c2bd410b71e8bc6ea2966de6e4c54b95b4e8
Jack Fuge <jack.fuge@gmail.com>
2018-09-27 11:50:51 +1000

    testing again...

--- www/queries.php
+++ www/queries.php
@@ -37,8 +37,6 @@ if (!isset($_GET['qid'])) {
                 $stmt = $conn->prepare($sql);
                 $stmt->execute();
 
-                echo "<table class=\"resultstable\">";
-                echo "<tr><th>prod_id</th><th>name</th><th>description</th><th>cost</th></tr>";
                 $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
                 foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
                     echo $v;
