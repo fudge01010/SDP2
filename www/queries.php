439a4e79f7f246bf53c9a3e8c26823897770087f
Jack Fuge <jack.fuge@gmail.com>
2018-09-27 11:49:44 +1000

    wtf2

--- www/queries.php
+++ www/queries.php
@@ -36,6 +36,9 @@ if (!isset($_GET['qid'])) {
                 $sql = "SELECT * FROM products";
                 $stmt = $conn->prepare($sql);
                 $stmt->execute();
+
+                echo "<table class=\"resultstable\">";
+                echo "<tr><th>prod_id</th><th>name</th><th>description</th><th>cost</th></tr>";
                 $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
                 foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
                     echo $v;
