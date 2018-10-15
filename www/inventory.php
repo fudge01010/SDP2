e43dbc322d1d9947efa9113581decc5ee591ca83
Kain Burgemeister <kain3012@hotmail.com>
2018-10-15 16:55:43 +1100

    lots of things. mainly hyperlink and patterns fixing

--- www/inventory.php
+++ www/inventory.php
@@ -21,8 +21,8 @@
 			}, function(data, status){
 			    var result = $.parseJSON(data);
 			    	$.each(result, function(i, field){
-			    		$("#resultstable").find("tbody:last").append("<tr><td>" + field.prod_id + "</td><td>" + field.name + "</td><td>" + field.description + "</td><td>" + field.cost + "</td></tr>");
-			    	});
+			    		$("#resultstable").find("tbody:last").append("<tr><td><a href=\"edititem.html\">" + field.prod_id + "</a></td><td>" + field.name + "</td><td>" + field.description + "</td><td>" + field.cost + "</td></tr>");
+      	});
 			    	// console.log(result);
 			    });
 		});
